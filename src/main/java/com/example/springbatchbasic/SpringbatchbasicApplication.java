7ede43ff3d44248a6f029198d317bdf60d7af703
SBSun <46879319+SBSun@users.noreply.github.com>
2023-06-30 18:51:58 +0900

    Project Setting

---
+++ src/main/java/com/example/springbatchbasic/SpringbatchbasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchbasicApplication.class, args);
+	}
+
+}
