38e146e2617f9d26481b695a5275c46a892bb4c3
SBSun <46879319+SBSun@users.noreply.github.com>
2023-07-01 01:30:15 +0900

    Feat : 간단한 스케줄러를 구현하여 2개의 Job 동작 테스트

---
+++ src/main/java/com/example/springbatchbasic/job/MyScheduler.java
@@ -0,0 +1,52 @@
+package com.example.springbatchbasic.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@EnableScheduling
+@Component
+public class MyScheduler {
+
+    @Autowired
+    private JobRegistry jobRegistry;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(){
+        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+
+        return jobRegistryBeanPostProcessor;
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    public void startHelloWorldJob() throws JobExecutionException {
+        JobParameters params = new JobParametersBuilder()
+                .addString("JobId", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("helloWorldJob"), params);
+    }
+
+    @Scheduled(fixedDelay = 20000)
+    public void startValidatedParamJob() throws JobExecutionException {
+        JobParameters params = new JobParametersBuilder()
+                .addString("JobId", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("validatedParamJob"), params);
+    }
+}
--- src/main/java/com/example/springbatchbasic/job/validatedparam/ValidatedParamJobConfig.java
+++ src/main/java/com/example/springbatchbasic/job/validatedparam/ValidatedParamJobConfig.java
@@ -1,7 +1,5 @@
-package com.example.springbatchbasic.job;
+package com.example.springbatchbasic.job.validatedparam;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -20,33 +18,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@Slf4j
-public class HelloWorldJobConfig{
+public class ValidatedParamJobConfig {
 
     @Bean
-    public Job helloWorldJob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
-        return new JobBuilder("helloWorldJob", jobRepository)
-                .incrementer(new RunIdIncrementer()) // Job을 실행할 때 Id를 부여하는데, Sequence를 순차적으로 부여할 수 있도록 설정
-                .start(helloWorldStep(jobRepository, platformTransactionManager))
+    public Job validatedParamJob(JobRepository jobRepository, Step validatedParamStep){
+        return new JobBuilder("validatedParamJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(validatedParamStep)
                 .build();
     }
 
     @JobScope
     @Bean
-    public Step helloWorldStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
-        return new StepBuilder("helloWorldStep", jobRepository)
-                .tasklet(helloWorldTasklet(), platformTransactionManager)
+    public Step validatedParamStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
+        return new StepBuilder("validatedParamStep", jobRepository)
+                .tasklet(validatedParamTasklet(), platformTransactionManager)
                 .build();
     }
 
     @StepScope
     @Bean
-    public Tasklet helloWorldTasklet(){
+    public Tasklet validatedParamTasklet(){
         return new Tasklet() {
             @Override
             public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                System.out.println("Hello World Spring Batch");
-                return RepeatStatus.FINISHED;   // FINISHED를 명시함으로써 이 스탭을 끝내도록 설정
+                System.out.println("validated Param Tasklet");
+                return RepeatStatus.FINISHED;
             }
         };
     }
